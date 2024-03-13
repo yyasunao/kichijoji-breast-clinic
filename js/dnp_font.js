8786923f8dc5dcd7b9cada0af6c27a18d8cf0477
yyasunao <yyasunao@gmail.com>
2024-03-13 11:35:08 +0900

    CSS更新

--- js/dnp_font.js
+++
@@ -1,9 +0,0 @@
-// DNP 秀英丸ゴシック Stdの読み込み
-(function(d) {
-  var config = {
-    kitId: 'tio3jay',
-    scriptTimeout: 3000,
-    async: true
-  },
-  h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-})(document);
