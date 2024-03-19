d11e570ebe4e4ffdaf9a6a8a943af966c2c85578
sugi <sugimoto_f@degitec.net>
2024-03-19 11:41:41 +0900

    診察券番号バリデーション無効

--- js/appointment.js
+++ js/appointment.js
@@ -265,11 +265,5 @@
 		}
 	}
 
-	// 診察券番号 バリデーション
-	if($('input[name=card-number]').length){
-		$('input[name=card-number]').addClass('validate[condRequired[card-number-required]]');
-		$('input[name=card-number-check]').eq(0).attr('id', 'card-number-required');
-	}
-	
 
 })(jQuery);
